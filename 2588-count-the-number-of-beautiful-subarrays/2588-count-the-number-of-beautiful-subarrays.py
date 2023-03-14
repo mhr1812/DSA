3cf26e3ca0434728a95db0b16e0bc81293047099
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-03-14 22:28:32 +0530

    Time: 1153 ms (43.08%), Space: 36 MB (76.71%) - LeetHub

--- 2588-count-the-number-of-beautiful-subarrays/2588-count-the-number-of-beautiful-subarrays.py
+++ 2588-count-the-number-of-beautiful-subarrays/2588-count-the-number-of-beautiful-subarrays.py
@@ -1,12 +1,8 @@
 class Solution:
     def beautifulSubarrays(self, nums: List[int]) -> int:
         n = len(nums)
-        pre_xor = [0 for i in range(n+1)]
-        count = [0 for i in range(1<<20)]
-        ans = 0
-        count[0] = 1
-        for i in range(1,n+1):
-            pre_xor[i] = pre_xor[i-1]^nums[i-1]
-            ans+=count[pre_xor[i]]
-            count[pre_xor[i]]+=1
-        return ans
+        cnt = Counter({0:1})
+        for val in accumulate(nums,operator.xor):
+            cnt[val]+=1
+        return sum(v*(v-1)//2 for v in cnt.values()) 
+        
