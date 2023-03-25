40a94a2c54370355011e3597a487d7c28991facf
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-03-25 21:11:49 +0530

    Time: 36 ms (64.72%), Space: 13.8 MB (93.75%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -1,7 +1,10 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        d = defaultdict(int)
-        for e in nums:
-            if e!=0:
-                d[e]+=1
-        return len(d)
+        # d = defaultdict(int)
+        # for e in nums:
+        #     if e!=0:
+        #         d[e]+=1
+        # return len(d)
+        nums1 = [e for e in nums if e>0]
+        c = Counter(nums1)
+        return len(c)
