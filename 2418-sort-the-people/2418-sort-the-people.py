6504dd9f5dc2f2870da6d89917a7c395728bca35
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-04-07 22:44:26 +0530

    Time: 109 ms (93.35%), Space: 14.3 MB (78.32%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,4 +1,7 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        _,names = zip(*sorted(zip(heights,names), reverse = True)) 
-        return  list(names)
+        height_dict = dict(zip(heights,names)) # // height_dict = {180: 'Mary', 165: 'John', 170: 'Emma'}
+        names.clear()
+        for key in sorted(height_dict.keys(),reverse=True):
+            names.append(height_dict[key])
+        return names
