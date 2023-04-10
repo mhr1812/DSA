e47b89b80c6cd8f10959b05d2e9aed042abcf116
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-04-10 22:02:15 +0530

    Time: 825 ms (15.38%), Space: 46.9 MB (69.23%) - LeetHub

--- 2615-sum-of-distances/2615-sum-of-distances.py
+++ 2615-sum-of-distances/2615-sum-of-distances.py
@@ -2,16 +2,31 @@ class Solution:
     def distance(self, nums: List[int]) -> List[int]:
         n = len(nums)
         a = [0 for i in range(n)]
-        c = Counter(nums)  
-        d = {}    
+        # c = Counter(nums)  
+        # d = {}    
+        # for i in range(n):
+        #     if nums[i] not in d:
+        #         d[nums[i]] = i
+        #     else:
+        #         d[nums[i]] += i
+        # for i in range(n):
+        #     a[i] = d[nums[i]]-c[nums[i]]*i
+        #     d[nums[i]]-=2*i
+        #     c[nums[i]]-=2
+        d = defaultdict(list)
         for i in range(n):
-            if nums[i] not in d:
-                d[nums[i]] = i
-            else:
-                d[nums[i]] += i
-        for i in range(n):
-            a[i] = d[nums[i]]-c[nums[i]]*i
-            d[nums[i]]-=2*i
-            c[nums[i]]-=2
+            d[nums[i]].append(i)
+        for num,val in d.items():
+            suffixSum=sum(val)
+            preffixSum=0
+            s=len(val)
+            p=0
+            for i in val:
+                preffixSum+=i
+                p+=1
+                suffixSum-=i
+                s-=1
+                a[i]=-preffixSum + p*i - s*i + suffixSum
+        return a    
         return a
                 
