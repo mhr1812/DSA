d8c8d79abb6ff460d17af4fd9f965b4b79c0f641
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-04-11 22:08:25 +0530

    Time: 1221 ms (74.66%), Space: 28.6 MB (61.51%) - LeetHub

---
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimizeMax(self, nums: List[int], p: int) -> int:
+        n = len(nums)
+        nums.sort()
+        mini_max_diff = 0
+        maxi_max_diff = nums[-1]-nums[0]
+        
+        while mini_max_diff<maxi_max_diff:
+            mid = (mini_max_diff+maxi_max_diff)//2
+            c,i = 0,1
+            
+            while i<n:
+                if nums[i]-nums[i-1]<=mid:
+                    c+=1
+                    i+=1
+                i+=1
+            
+            if c>=p:
+                maxi_max_diff = mid
+            else:
+                mini_max_diff = mid+1
+            
+        return mini_max_diff
