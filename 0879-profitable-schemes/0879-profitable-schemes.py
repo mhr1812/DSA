696c94392bd0aa5cd453e637677888aa546594c3
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-04-21 18:51:55 +0530

    Time: 1589 ms (70.48%), Space: 14.1 MB (99.05%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.py
+++ 0879-profitable-schemes/0879-profitable-schemes.py
@@ -1,17 +1,13 @@
 class Solution:
     def profitableSchemes(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
-        m=len(group)
-        dp=[[[0]*(minProfit+1) for _ in range(n+1)] for _ in range(m+1)]
-        for j in range(n+1):
-            dp[m][j][0]=1
-
-        for i in range(m-1,-1,-1):
-            for j in range(n+1):
-                for k in range(minProfit+1):
-                    dp[i][j][k]=dp[i+1][j][k]
-                    if group[i]<=j:
-                        dp[i][j][k]+=dp[i+1][j-group[i]][max(0,k-profit[i])]
-                    dp[i][j][k]%=10**9+7
-
-        return dp[0][n][minProfit]
+        mod = int(1e9) + 7
+        dp = [[0] * (minProfit + 1) for _ in range(n + 1)]
+        dp[0][0] = 1
+        for k in range(1, len(group) + 1):
+            g = group[k - 1]
+            p = profit[k - 1]
+            for i in range(n, g - 1, -1):
+                for j in range(minProfit, -1, -1):
+                    dp[i][j] = (dp[i][j] + dp[i - g][max(0, j - p)]) % mod
+        return sum(dp[i][minProfit] for i in range(n + 1)) % mod
         
