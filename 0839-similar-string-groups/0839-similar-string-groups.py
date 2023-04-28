19e6bb548e4a9d4a435b06d30cb9583b7850457e
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-04-28 21:51:35 +0530

    Time: 2051 ms (24.59%), Space: 16.7 MB (10.16%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.py
+++ 0839-similar-string-groups/0839-similar-string-groups.py
@@ -1,36 +1,23 @@
-#from collections import deque
 class Solution:
     def numSimilarGroups(self, strs: List[str]) -> int:
-        def is_similar(a,b):
-            c = 0
-            for i in range(len(a)):
-                if a[i]!=b[i]:
-                    c+=1
-            return c==2 or c==0 
-    
+        groups = 0
         n = len(strs)
-        graph = [[] for i in range(n)]
-        
+        vis = [False] * n
         for i in range(n):
-            for j in range(i+1,n):
-                if is_similar(strs[i],strs[j]):
-                    graph[i].append(j)
-                    graph[j].append(i)
-        
-        visited = [False for i in range(n)]
-        ans = 0
-        
-        for i in range(n):
-            if visited[i]: continue
-            ans+=1
-            q = deque([i])
-            
-            while q:
-                node = q.popleft()
-                if visited[node]: continue
-                visited[node] = True
-                
-                for v in graph[node]:
-                    if visited[v]: continue
-                    q.append(v)
-        return ans
+            if vis[i]: continue
+            groups += 1
+            self.dfs(i, strs, vis)
+        return groups
+
+    def dfs(self, i: int, strs: List[str], vis: List[bool]) -> None:
+        vis[i] = True
+        for j in range(len(strs)):
+            if vis[j]: continue
+            if self.is_similar(strs[i], strs[j]):
+                self.dfs(j, strs, vis)
+
+    def is_similar(self, a: str, b: str) -> bool:
+        count = 0
+        for i in range(len(a)):
+            if a[i] != b[i]: count += 1
+        return count == 2 or count == 0
