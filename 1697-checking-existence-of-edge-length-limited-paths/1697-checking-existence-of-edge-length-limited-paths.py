0793de2423e1ee1b6507d74fdafa96c05b1c3760
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-04-29 21:48:16 +0530

    Time: 1844 ms (97.48%), Space: 62.9 MB (7.55%) - LeetHub

--- 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.py
+++ 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.py
@@ -1,36 +1,27 @@
-class UnionFind:
-    def __init__(self, N: int):
-        self.parent = list(range(N))
-        self.rank = [1] * N
-
-    def find(self, p: int) -> int:
-        if p != self.parent[p]:
-            self.parent[p] = self.find(self.parent[p])
-        return self.parent[p]
-
-    def union(self, p: int, q: int) -> bool:
-        prt, qrt = self.find(p), self.find(q)
-        if prt == qrt: return False 
-        if self.rank[prt] > self.rank[qrt]: 
-            prt, qrt = qrt, prt 
-        self.parent[prt] = qrt 
-        self.rank[qrt] += self.rank[prt] 
-        return True 
-
-
 class Solution:
-    def distanceLimitedPathsExist(self, n: int, edgeList: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        queries = sorted((w, p, q, i) for i, (p, q, w) in enumerate(queries))
-        edgeList = sorted((w, u, v) for u, v, w in edgeList)
-        
-        uf = UnionFind(n)
-        
-        ans = [None] * len(queries)
-        ii = 0
-        for w, p, q, i in queries: 
-            while ii < len(edgeList) and edgeList[ii][0] < w: 
-                _, u, v = edgeList[ii]
-                uf.union(u, v)
-                ii += 1
-            ans[i] = uf.find(p) == uf.find(q)
-        return ans 
+    def distanceLimitedPathsExist(self, n: int, edges: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        # add index to queries:
+        queries = [(d, a, b, ind) for ind, (a, b, d) in enumerate(queries)]
+        # sort by distance cutoff:
+        queries.sort(key = lambda x: x[0])
+        # sort edges by distance:
+        edges.sort(key = lambda x: x[2])
+        edgeI = 0
+        parent = [i for i in range(n)]
+        def root(x):
+            while x != parent[x]:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+        def union(x, y):
+            x = root(x)
+            y = root(y)
+            parent[x] = y
+        ret = [False for _ in range(len(queries))]
+        for queryDist, a, b, ind in queries:
+            # first union all edges with distance < queryDist
+            while edgeI < len(edges) and edges[edgeI][2] < queryDist:
+                union(edges[edgeI][0], edges[edgeI][1])
+                edgeI += 1
+            ret[ind] = (root(a) == root(b))
+        return ret
