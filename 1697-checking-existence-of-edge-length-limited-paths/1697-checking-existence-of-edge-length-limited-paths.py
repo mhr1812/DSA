5d049841d6f0e5d346368a4a5e521dd24889dc33
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-04-29 21:46:22 +0530

    Time: 2077 ms (25.79%), Space: 62.6 MB (7.55%) - LeetHub

--- 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.py
+++ 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.py
@@ -1,36 +1,36 @@
-class DSU(object):
-    def __init__(self, n):
-        self.parents = [i for i in range(n)]
-    
-    def find(self,x):
-        if self.parents[x] != x:
-            self.parents[x] = self.find(self.parents[x])
-        return self.parents[x]
-    
-    def union(self, x, y):
-        rootA, rootB = self.find(x), self.find(y)
-        if rootA != rootB:
-            self.parents[rootA] = rootB
-            
+class UnionFind:
+    def __init__(self, N: int):
+        self.parent = list(range(N))
+        self.rank = [1] * N
+
+    def find(self, p: int) -> int:
+        if p != self.parent[p]:
+            self.parent[p] = self.find(self.parent[p])
+        return self.parent[p]
+
+    def union(self, p: int, q: int) -> bool:
+        prt, qrt = self.find(p), self.find(q)
+        if prt == qrt: return False 
+        if self.rank[prt] > self.rank[qrt]: 
+            prt, qrt = qrt, prt 
+        self.parent[prt] = qrt 
+        self.rank[qrt] += self.rank[prt] 
+        return True 
+
+
 class Solution:
     def distanceLimitedPathsExist(self, n: int, edgeList: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        dsu = DSU(n)
-        for i, q in enumerate(queries):
-            queries[i].append(i)
-
-        queries.sort(key=lambda q: q[2])
-        edgeList.sort(key=lambda e: e[2])
-
-        i = 0
-        res = [False] * len(queries)
-        for q in queries:
-            while i < len(edgeList) and edgeList[i][2] < q[2]:
-                dsu.union(edgeList[i][0], edgeList[i][1])
-                i += 1
-
-            if dsu.find(q[0]) == dsu.find(q[1]):
-                res[q[3]] = True
-
-        return res
-
+        queries = sorted((w, p, q, i) for i, (p, q, w) in enumerate(queries))
+        edgeList = sorted((w, u, v) for u, v, w in edgeList)
         
+        uf = UnionFind(n)
+        
+        ans = [None] * len(queries)
+        ii = 0
+        for w, p, q, i in queries: 
+            while ii < len(edgeList) and edgeList[ii][0] < w: 
+                _, u, v = edgeList[ii]
+                uf.union(u, v)
+                ii += 1
+            ans[i] = uf.find(p) == uf.find(q)
+        return ans 
