6aa2fbd41eec8df44ba8ec53d4b3bc08c05425d7
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-04-30 17:25:36 +0530

    Time: 2124 ms (79.87%), Space: 56.7 MB (18.24%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.py
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.py
@@ -1,86 +1,36 @@
-class DSU:
-    def __init__(self, num):
-        self.parents = [-1] * (num + 1)
-        
-    def findParent(self, i):
-        if self.parents[i] < 0:
-            return i
-        
-        else:
-            self.parents[i] = self.findParent(self.parents[i])
-            
-        return self.parents[i]
-    
-    def union(self, i, j):
-        iParent = self.findParent(i)
-        jParent = self.findParent(j)
-        
-        if iParent == jParent:
-            return False
-        
-        if abs(self.parents[iParent]) >= abs(self.parents[jParent]):
-            self.parents[iParent] += self.parents[jParent]
-            self.parents[jParent] = iParent
-            
-        else:
-            self.parents[jParent] += self.parents[iParent]
-            self.parents[iParent] = jParent
-            
-        return True
+def find(reps, u):
+    if reps[u] == -1:
+        return u
+    reps[u] = find(reps, reps[u])
+    return reps[u]
+
+def union(reps, u, v):
+    rep_u = find(reps, u)
+    rep_v = find(reps, v)
+    if rep_u != rep_v:
+        reps[rep_u] = rep_v
 
 class Solution:
     def maxNumEdgesToRemove(self, n: int, edges: List[List[int]]) -> int:
-        alice = DSU(n)
-        bob = DSU(n)
+        m3 = 0 # number of type 3 edges added
+        reps = [-1] * (n + 1)
+        for t, u, v in edges:
+            if t == 3 and find(reps, u) != find(reps, v):
+                union(reps, u, v)
+                m3 += 1
         
-        onlyAlice = []
-        onlyBob = []
-        both = []
+        m1 = 0 # number of type 1 edges added
+        m2 = 0 # number of type 2 edges added
+        reps2 = reps.copy()
+        for t, u, v in edges:
+            if t == 1 and find(reps, u) != find(reps, v):
+                union(reps, u, v)
+                m1 += 1
+            elif t == 2 and find(reps2, u) != find(reps2, v):
+                union(reps2, u, v)
+                m2 += 1
         
-        for x, y, z in edges:
-            if x == 1:
-                onlyAlice.append([y, z])
-                
-            elif x == 2:
-                onlyBob.append([y, z])
-                
-            else:
-                both.append([y, z])
-                
-        ans = 0
+        if m1 + m3 != n - 1 or m2 + m3 != n - 1:
+            return -1
         
-        for x, y in both:
-            case1 = alice.union(x, y)
-            case2 = bob.union(x, y)
-            
-            if not case1 and not case2:
-                ans += 1
-                
-        for x, y in onlyAlice:
-            if not alice.union(x, y):
-                ans += 1
-                
-        for x, y in onlyBob:
-            if not bob.union(x, y):
-                ans += 1
-            
-        #print(alice.parents)
-        #print(bob.parents)
-        
-        cnt = 0
-        for elem in alice.parents[1:]:
-            if elem < 0:
-                cnt += 1
-                
-            if cnt > 1:
-                return -1
-        
-        cnt = 0
-        for elem in bob.parents[1:]:
-            if elem < 0:
-                cnt += 1
-                
-            if cnt > 1:
-                return -1
-        
-        return ans
+        return len(edges) - (m1 + m2 + m3)
