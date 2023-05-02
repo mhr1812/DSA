55d9b2c42a8d7e1b06cc2687e1cd40dbed0f890b
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-05-02 09:30:10 +0530

    Time: 75 ms (5.64%), Space: 16.6 MB (7.53%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
@@ -1,4 +1,14 @@
 class Solution:
-    def arraySign(self, nums: List[int]) -> int:
-        return 0 if 0 in nums else reduce(mul, [-1 if num < 0 else 1 for num in nums])
-        
+	def arraySign(self, nums: List[int]) -> int:
+		nums, count = sorted(nums), 0
+		for i in range(len(nums)):
+			if nums[i] == 0:
+				return 0
+			if nums[i] > 0:
+				if count % 2 == 0: return 1 
+				else: return -1
+			count+=1
+		
+		if count % 2 == 0: # if all are negative
+			return 1
+		else: return -1
