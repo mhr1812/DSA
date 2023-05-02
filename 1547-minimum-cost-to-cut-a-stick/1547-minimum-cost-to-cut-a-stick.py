ab8bbd2e4be9820dc1a1a2bec42fce8720e16f82
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-05-02 18:39:36 +0530

    Time: 923 ms (84.23%), Space: 16.6 MB (43.62%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
@@ -2,26 +2,15 @@ class Solution:
     def minCost(self, n: int, cuts: List[int]) -> int:
         cuts.sort()
         A = [0] + cuts + [n]
+        dp = [[0 for j in range(len(cuts)+2)] for i in range(len(cuts)+2)]
         
-        def dfs(i, j, A, dp):
-            if i > j:
-                return 0
-			# check the memoization cache
-            if dp[i][j] != -1:
-                return dp[i][j]
-            
-            mini = float("inf")
-            for k in range(i, j+1):
-                cost = A[j+1]-A[i-1] + dfs(i, k-1, A, dp) + dfs(k+1, j, A, dp)
-                mini = min(cost, mini)
-            
-			# set the computed value so we don't have to recompute
-            dp[i][j] = mini
-            return mini
-                
-		# build our len(cuts)*len(cuts) 2D array cache
-        dp = [[-1 for j in range(len(cuts)+1)] for i in range(len(cuts)+1)]
-        
-        res = dfs(1, len(cuts), A, dp)
-        return res
-        
+        for i in range(len(cuts), 0, -1):
+            for j in range(1, len(cuts)+1):
+                if i > j:
+                    continue
+                mini = float("inf")
+                for k in range(i, j+1):
+                    cost = A[j+1]-A[i-1] + dp[i][k-1] + dp[k+1][j]
+                    mini = min(cost, mini)
+                dp[i][j] = mini
+        return dp[1][len(cuts)]
