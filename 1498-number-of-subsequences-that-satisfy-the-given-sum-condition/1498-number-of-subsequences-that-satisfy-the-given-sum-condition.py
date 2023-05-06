1687b74714cc0a3e8780303bd4b2967a714d62f1
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-05-06 19:25:38 +0530

    Time: 1370 ms (57.93%), Space: 28.6 MB (13.68%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
@@ -1,13 +1,28 @@
 m = int(1e9+7)
 class Solution:
+    
+        
     def numSubseq(self, nums: List[int], target: int) -> int:
         n = len(nums)
         low,high = 0,n-1
         nums.sort()
         ans = 0
+        def power(a,b):
+            if b==0:
+                return 1
+            if b==1:
+                return a
+            ans = 0
+            if b%2==0:
+                ans = power(a,b//2)
+                ans*=ans
+            else:
+                ans = power(a,b-1)
+                ans*=a
+            return ans%m
         while low<=high:
             if nums[low]+nums[high]<=target:
-                ans+=pow(2,high-low)%m
+                ans+=power(2,high-low)
                 ans%=m
                 low+=1
             else:
