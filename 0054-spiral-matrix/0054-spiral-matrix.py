1f8eacef5b540c4031e23cb892a6911ded8ce83a
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-05-09 16:02:24 +0530

    Time: 50 ms (5.97%), Space: 16.2 MB (14.84%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,4 +1,63 @@
 class Solution:
-    def spiralOrder(self, x: List[List[int]]) -> List[int]:
-        return x and [*x.pop(0)] + self.spiralOrder([*zip(*x)][::-1])
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        m = len(matrix)
+        n = len(matrix[0])
+        
+        i = j = 0
+        
+        right = True
+        left = False
+        down = False
+        up = False
+        
+        res = []
+        cnt = 0
+        
+        while cnt < m*n:
             
+            ##STOP RIGHT GO DOWN
+            if right and (j == n-1):
+                right = False
+                down = True
+            elif right and matrix[i][j+1] == None:
+                right = False
+                down = True
+                
+            ##STOP DOWN GO LEFT    
+            elif down and (i == m-1):
+                down = False
+                left = True
+            elif down and matrix[i+1][j] == None:
+                down = False
+                left = True
+                
+            ##STOP LEFT GO UP    
+            elif left and (j == 0):
+                left = False
+                up = True
+            elif left and matrix[i][j-1] == None:
+                left = False
+                up = True
+                
+            #STOP UP GO RIGHT    
+            elif up and (i == 0):
+                up = False
+                right = True
+            elif up and matrix[i-1][j] == None:
+                up = False
+                right = True    
+            
+            #store the result
+            res.append(matrix[i][j])
+            cnt += 1
+            
+            #Block the place
+            matrix[i][j] = None
+            
+            #NEXT MOVES
+            if right: j += 1
+            elif left: j -= 1
+            elif down : i += 1
+            elif up: i -= 1
+            
+        return res
