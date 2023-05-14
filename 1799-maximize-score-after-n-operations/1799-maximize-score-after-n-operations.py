b4e7dd7638a2ec882cc2d58d675124270706d4d5
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-05-14 07:11:49 +0530

    Time: 1432 ms (94.30%), Space: 22.5 MB (66.46%) - LeetHub

--- 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.py
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.py
@@ -1,23 +1,17 @@
 class Solution:
     def maxScore(self, nums: List[int]) -> int:
-        def gcd(x, y):
-            return math.gcd(x, y)
+        n = len(nums)
+        gcds = {(j, k): gcd(nums[j], nums[k]) for j, k in combinations(range(n), 2)}
 
         @lru_cache(None)
-        def dp(op, mask):
-            if op == n + 1:  # Reach to n operations
-                return 0
+        def dfs(bitmask):
+            if bitmask == 0: return 0
+            
+            cand = 0
+            n_z_bits = [j for j in range(n) if bitmask&(1<<j)]
+            for j, k in combinations(n_z_bits, 2):
+                q = bitmask^(1<<j)^(1<<k)
+                cand = max(cand, dfs(q) + (n+2 - len(n_z_bits))//2*gcds[j, k])
+            return cand
 
-            ans = 0
-            for i in range(m):
-                if (mask >> i) & 1: continue  # If nums[i] is used -> Skip
-                for j in range(i + 1, m):
-                    if (mask >> j) & 1: continue  # If nums[j] is used -> Skip
-                    newMask = (1 << i) | (1 << j) | mask  # Mark nums[i] and nums[j] as used!
-                    score = op * gcd(nums[i], nums[j]) + dp(op + 1, newMask)
-                    ans = max(ans, score)
-            return ans
-
-        m = len(nums)
-        n = m // 2
-        return dp(1, 0)
+        return dfs((1<<n) - 1)
