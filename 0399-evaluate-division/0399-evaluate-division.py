9b7120b28c5cea3d647bcbbcc75f0344c11ba590
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-05-20 10:56:18 +0530

    Time: 41 ms (38.17%), Space: 16.3 MB (25.75%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,28 +1,26 @@
 class Solution:
-    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        def dfs(graph, start, end, visited):
-            if start == end and graph[start]:
-                return 1.0
-            
-            visited.add(start)
-            for neigh, val in graph[start]:
-                if neigh in visited:
-                    continue
-                
-                tmp = dfs(graph, neigh, end, visited)
-                if tmp > 0:
-                    return val * tmp
-            
-            return -1.0
-            
-        graph = collections.defaultdict(set)
-        for items, v in zip(equations, values):
-            x, y = items
-            graph[x].add((y, v))
-            graph[y].add((x, 1.0 / v))
+    def find(self, graph, start, end, res, visited):
+        if start == end:return res
+        if start in visited or start not in graph:return -1
+        visited[start]=1
+        for i in graph[start]:
+            temp = self.find(graph, i[0], end, res*i[1], visited)
+            if temp != -1: return temp   
+        return -1
         
-        res = []
+    def search(self, graph, q):
+        start, end = q[0], q[1]
+        if start not in graph or end not in graph: return -1
+        return self.find(graph, start, end, 1, {})
+    
+    def calcEquation(self, equations, values, queries):
+        res, aList = [], collections.defaultdict(list)
+        
+        for eq,val in zip(equations,values):
+            aList[eq[0]].append((eq[1],val))
+            aList[eq[1]].append((eq[0],1/val))
+        
         for q in queries:
-            res.append(dfs(graph, q[0], q[1], set()))
-        
+            res.append(self.search(aList,q))
+            
         return res
