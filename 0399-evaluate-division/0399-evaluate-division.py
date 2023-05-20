13922edef979efdd05de54fd0632640734e5b83b
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-05-20 11:01:21 +0530

    Time: 44 ms (31.85%), Space: 16.4 MB (25.75%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,45 +1,34 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        adj = defaultdict(list)
-        for pos,i in enumerate(equations):
-            adj[i[0]].append((i[1],values[pos]))
-            adj[i[1]].append((i[0],1/values[pos]))
-            
-        #  0 -> false
-        #  1 -> true
-        # -1 -> not found
-        
-        def value_f():
-            return -1
-        
-        def createdict(visited):
-            for pair in equations:
-                visited[pair[0]] = 0
-                visited[pair[1]] = 0
-        
-        output = []
-        def dfs(start,END,res):
-            if visited[start] == 1:
-                return
-            visited[start] = 1 # is Visited
-            if start == END:   #reach to endpoints
-                if len(adj[start]) != 0:
-                    output.append(res)
-                return
+        graph=defaultdict(dict)
+        n=len(equations)
+        for i in range(n):
+            graph[equations[i][0]][equations[i][1]]=values[i]
+            graph[equations[i][1]][equations[i][0]]=1/values[i]
 
-            for s_to in adj[start]:
-                if visited[s_to[0]] == 0:
-                    # print(start,"->",s_to[0],END,res * s_to[1])
-                    dfs(s_to[0],END,res * s_to[1])       
-                    
-        for pos,pair in enumerate(queries):
-            visited = defaultdict(value_f)
-            createdict(visited)
-            if(visited[pair[0]] == -1 or visited[pair[1]] == -1): #that point not appear in graph
-                output.append(-1)
-                continue
-            dfs(pair[0],pair[1],1)   #traverse by DFS
-            if len(output) < pos + 1: # append nothing after executing dfs() 
-                output.append(-1)
-            
-        return output
+        def dfs(x,y,visited):
+            if x not in graph or y not in graph:
+                return -1
+
+            if y in graph[x]:
+                return graph[x][y]
+
+            for i in graph[x]:
+                if i not in visited:
+                    visited.add(i)
+                    temp=dfs(i,y,visited)
+
+                    if temp==-1:
+                        continue
+
+                    else:
+                        return temp*graph[x][i]
+
+            return -1
+
+
+        ans=[]
+        for p,q in queries:
+            ans.append(dfs(p,q,set()))
+
+        return ans             
