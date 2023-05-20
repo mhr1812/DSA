94361d2a6ec94aed4c99645ce09d2dafa8f256e8
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-05-20 11:00:10 +0530

    Time: 48 ms (23.02%), Space: 16.5 MB (8.30%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,37 +1,45 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        def bellman_ford(src, trg):
-            if src == trg:
-                return 1.
+        adj = defaultdict(list)
+        for pos,i in enumerate(equations):
+            adj[i[0]].append((i[1],values[pos]))
+            adj[i[1]].append((i[0],1/values[pos]))
             
-            V = len(vertices)
-            dist = {v: float('inf') for v in vertices}
-            dist[src] = 1.
-            
-            for i in range(V - 1):
-                for u, v, w in edges:
-                    dist[v] = min(dist[v], dist[u] * w)
-            
-            return dist[trg] if dist[trg] != float('inf') else -1.
+        #  0 -> false
+        #  1 -> true
+        # -1 -> not found
         
-        n = len(equations)
-        vertices = set()
-        edges = []
+        def value_f():
+            return -1
         
-        for i in range(n):
-            a, b = equations[i]
-            val = values[i]
+        def createdict(visited):
+            for pair in equations:
+                visited[pair[0]] = 0
+                visited[pair[1]] = 0
+        
+        output = []
+        def dfs(start,END,res):
+            if visited[start] == 1:
+                return
+            visited[start] = 1 # is Visited
+            if start == END:   #reach to endpoints
+                if len(adj[start]) != 0:
+                    output.append(res)
+                return
+
+            for s_to in adj[start]:
+                if visited[s_to[0]] == 0:
+                    # print(start,"->",s_to[0],END,res * s_to[1])
+                    dfs(s_to[0],END,res * s_to[1])       
+                    
+        for pos,pair in enumerate(queries):
+            visited = defaultdict(value_f)
+            createdict(visited)
+            if(visited[pair[0]] == -1 or visited[pair[1]] == -1): #that point not appear in graph
+                output.append(-1)
+                continue
+            dfs(pair[0],pair[1],1)   #traverse by DFS
+            if len(output) < pos + 1: # append nothing after executing dfs() 
+                output.append(-1)
             
-            vertices.add(a)
-            vertices.add(b)
-            
-            edges.append((a, b, val))
-            edges.append((b, a, 1 / val))
-        
-        res = [-1.] * len(queries)
-        for i, query in enumerate(queries):
-            c, d = query
-            if c in vertices and d in vertices:
-                res[i] = bellman_ford(c, d)
-        
-        return res
+        return output
