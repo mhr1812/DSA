488bae0892ba513e3bae331a87a2a4737fd51bb4
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-05-20 11:02:28 +0530

    Time: 43 ms (33.19%), Space: 16.5 MB (8.30%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,30 +1,25 @@
 class Solution:
+    def solver(self,x,y,graph,visited):
+        if x not in graph:
+            return -1.0
+        if x==y:
+            return 1
+        visited.add(x)  
+        for key,val in graph[x]:
+            if key not in visited:
+                crnt=self.solver(key,y,graph,visited)
+                if crnt!=-1:
+                    return val*crnt
+        return -1.0
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        graph=defaultdict(dict)
-        n=len(equations)
-        for i in range(n):
-            graph[equations[i][0]][equations[i][1]]=values[i]
-            graph[equations[i][1]][equations[i][0]]=1/values[i]
-
-        def dfs(x,y,visited):
-            if x not in graph or y not in graph:
-                return -1
-
-            if y in graph[x]:
-                return graph[x][y]
-
-            for i in graph[x]:
-                if i not in visited:
-                    visited.add(i)
-                    temp=dfs(i,y,visited)
-
-                    if temp==-1:
-                        continue
-
-                    else:
-                        return temp*graph[x][i]
-
-            return -1
+        graph=defaultdict(set)
+        for [u,v],val in zip(equations,values):
+            graph[u].add((v,val))
+            graph[v].add((u,1/val))
+        out=[]
+        for c,d in queries:
+            out.append(self.solver(c,d,graph,set()))
+        return out
 
 
         ans=[]
