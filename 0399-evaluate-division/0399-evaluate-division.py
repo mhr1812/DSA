2ce72ce00ccf9023ee6a3a9f895d40fc7ca3d002
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-05-20 10:54:19 +0530

    Time: 47 ms (24.68%), Space: 16.4 MB (25.75%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,23 +1,28 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        root = {}
-	
-	# xr = x/parent(x), pr = parent(x)/root(x), update xr to xr*pr = x/root(x)
-        def find(x):
-            p, xr = root.setdefault(x, (x, 1.0))
-            if x != p:
-                r, pr = find(p)
-                root[x] = (r, xr*pr)
-            return root[x]
-
-        # if root(x) = root(y), equations "x / y" doable as (x/root(x)) / (y/root(y)) = xr / yr
-        def union(x, y, ratio):
-            px, xr, py, yr = *find(x), *find(y)
-            if not ratio:
-                return xr / yr if px == py else -1.0
-            if px != py:
-                root[px] = (py, yr/xr*ratio)
-
-        for (x, y), v in zip(equations, values):
-            union(x, y, v)
-        return [union(x, y, 0) if x in root and y in root else -1.0 for x, y in queries]
+        def dfs(graph, start, end, visited):
+            if start == end and graph[start]:
+                return 1.0
+            
+            visited.add(start)
+            for neigh, val in graph[start]:
+                if neigh in visited:
+                    continue
+                
+                tmp = dfs(graph, neigh, end, visited)
+                if tmp > 0:
+                    return val * tmp
+            
+            return -1.0
+            
+        graph = collections.defaultdict(set)
+        for items, v in zip(equations, values):
+            x, y = items
+            graph[x].add((y, v))
+            graph[y].add((x, 1.0 / v))
+        
+        res = []
+        for q in queries:
+            res.append(dfs(graph, q[0], q[1], set()))
+        
+        return res
