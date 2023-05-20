db5eb218f9812e1c27617fe17da8de8b3eb4432f
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-05-20 10:53:39 +0530

    Time: 50 ms (15.90%), Space: 16.3 MB (25.75%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,23 +1,23 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        g = defaultdict(dict)
-        
-        for (x,y),v in zip(equations,values):
-            g[x][y] = v
-            g[y][x] = 1/v
-            
-        def bfs(src,dst):
-            if not(src in g and dst in g):
-                return -1.0
-            q = [(src,1.0)]
-            visited = set()
-            
-            for x,v in q:
-                if x==dst:
-                    return v
-                visited.add(x)
-                for y in g[x]:
-                    if y not in visited:
-                        q.append((y,v*g[x][y]))
-            return -1.0
-        return [bfs(s,d) for s,d in queries]
+        root = {}
+	
+	# xr = x/parent(x), pr = parent(x)/root(x), update xr to xr*pr = x/root(x)
+        def find(x):
+            p, xr = root.setdefault(x, (x, 1.0))
+            if x != p:
+                r, pr = find(p)
+                root[x] = (r, xr*pr)
+            return root[x]
+
+        # if root(x) = root(y), equations "x / y" doable as (x/root(x)) / (y/root(y)) = xr / yr
+        def union(x, y, ratio):
+            px, xr, py, yr = *find(x), *find(y)
+            if not ratio:
+                return xr / yr if px == py else -1.0
+            if px != py:
+                root[px] = (py, yr/xr*ratio)
+
+        for (x, y), v in zip(equations, values):
+            union(x, y, v)
+        return [union(x, y, 0) if x in root and y in root else -1.0 for x, y in queries]
