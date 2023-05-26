e1411036a453c5fcc85d127dbbeec9c55a1876fd
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-05-26 12:24:15 +0530

    Time: 237 ms (77.42%), Space: 16.7 MB (61.75%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.py
+++ 1140-stone-game-ii/1140-stone-game-ii.py
@@ -1,24 +1,28 @@
 class Solution:
-    def stoneGameII(self, piles: List[int]) -> int:
-        t = {}
-        def dfs(alice,i,M):
-            if i==len(piles):
-                return 0 
-            if (alice,i,M) in t:
-                return t[(alice,i,M)]
-            
-            ans = 0 if alice else float('inf')
-            sm = 0
-            for x in range(1,2*M+1):
-                if i+x-1==len(piles):
-                    break
-                sm+=piles[i+x-1]
-                if alice:
-                    ans = max(ans,sm+dfs(not alice,i+x,max(M,x)))
-                else:
-                    ans = min(ans,dfs(not alice,i+x,max(M,x)))
-            t[(alice,i,M)] = ans
-             
-            return ans
-        
-        return dfs(True,0,1)
+    def helper(self, piles, dp, suffixSum, i, M):
+        if i == len(piles):
+            return 0
+        if i + 2 * M >= len(piles):
+            return suffixSum[i]
+
+        if dp[i][M] != 0:
+            return dp[i][M]
+
+        result = 0
+        for x in range(1, 2 * M + 1):
+            result = max(result, suffixSum[i] - self.helper(piles, dp, suffixSum, i + x, max(M, x)))
+
+        dp[i][M] = result
+        return result
+
+    def stoneGameII(self, piles):
+        if not piles:
+            return 0
+        dp = [[0] * len(piles) for _ in range(len(piles))]
+
+        suffixSum = [0] * len(piles)
+        suffixSum[-1] = piles[-1]
+        for i in range(len(piles) - 2, -1, -1):
+            suffixSum[i] = piles[i] + suffixSum[i + 1]
+
+        return self.helper(piles, dp, suffixSum, 0, 1)
