ca6a4ae12fdf4b42fe9126ca13f2607682420439
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-06-01 10:39:44 +0530

    Time: 719 ms (34.48%), Space: 16.7 MB (52.41%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -4,14 +4,14 @@ class Solution:
         if grid[0][0]==1 or grid[-1][-1]==1:
             return -1
         n = len(grid)
-        q = Queue()
+        q = deque()
         dx = [-1,0,1]
         dy = [-1,0,1]
-        q.put([0,0,1])
+        q.append([0,0,1])
         grid[0][0] = 1
         
-        while not q.empty():
-            x,y,ans = q.get()
+        while q:
+            x,y,ans = q.popleft()
             if x==n-1 and y==n-1:
                 return ans
             
@@ -21,7 +21,7 @@ class Solution:
                     ny = y+dy[j]
                     
                     if 0<=nx<=n-1 and 0<=ny<=n-1 and grid[nx][ny]==0:
-                        q.put([nx,ny,ans+1])
+                        q.append([nx,ny,ans+1])
                         grid[nx][ny] = 1
         return -1
                 
