3ba6f360f552e9b57eca70df99e06df37f44f3f5
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-06-11 13:24:01 +0530

    Time: 132 ms (100.00%), Space: 21.1 MB (11.11%) - LeetHub

--- 2734-lexicographically-smallest-string-after-substring-operation/2734-lexicographically-smallest-string-after-substring-operation.py
+++ 2734-lexicographically-smallest-string-after-substring-operation/2734-lexicographically-smallest-string-after-substring-operation.py
@@ -1,13 +1,34 @@
 class Solution:
     def smallestString(self, s: str) -> str:
-        i = 0
-        n = len(s)
-        s = list(s)
-        while i < n and s[i] == 'a':
-            i += 1
-        if i == n:
-            s[-1] = 'z'
-        while i < n and s[i] != 'a':
-            s[i] = chr(ord(s[i]) - 1)
-            i += 1
-        return ''.join(s)
+        def onego(s):
+            return "".join(chr(ord(x)-1) for x in s)
+
+        if s == 'a':
+            return 'z'
+        elif 'a' not in s:
+            return onego(s)
+        elif s.count('a') == 1:
+            if s[0] == 'a':
+                return 'a'+onego(s[1:])
+            else:
+                return onego(s[:s.index('a')])+s[s.index('a'):]
+        else:
+            if s[0] != 'a':
+                return onego(s[:s.index('a')])+s[s.index('a'):]
+
+            elif len(s) == s.count('a'):
+                return s[:-1]+'z'
+            else:
+                idx = 0
+                for i in range(len(s)):
+                    if s[i] == 'a':
+                        continue
+                    else:
+                        idx = i
+                        break
+                if 'a' not in s[idx:]:
+                    return s[:idx]+onego(s[idx:])
+                else:
+                    ss = s[idx:]
+                    ii = idx + ss.index('a')
+                    return s[:idx]+onego(s[idx:ii])+s[ii:]
