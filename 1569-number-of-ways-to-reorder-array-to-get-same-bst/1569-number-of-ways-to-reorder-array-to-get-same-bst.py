0595eacc98aa0627a7644cda074ba5f5c6386ad2
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-06-16 18:51:51 +0530

    Time: 162 ms (94.56%), Space: 21.7 MB (79.35%) - LeetHub

---
+++ 1569-number-of-ways-to-reorder-array-to-get-same-bst/1569-number-of-ways-to-reorder-array-to-get-same-bst.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numOfWays(self, nums: List[int]) -> int:
+        mod = int(1e9+7)
+        def recur(nums):
+            if len(nums)<=2:
+                return 1
+            left = [e for e in nums if e<nums[0]]
+            right = [e for e in nums if e>nums[0]]
+            return comb(len(nums)-1,len(right))*recur(left)*recur(right)
+        ans = (recur(nums) -1)%mod
+        return ans
