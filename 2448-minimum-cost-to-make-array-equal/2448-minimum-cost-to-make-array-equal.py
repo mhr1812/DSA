7c44cf2c4009b6cf3f3f675edd94a44ab46f32cc
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-06-21 20:13:28 +0530

    Time: 402 ms (97.06%), Space: 35.1 MB (41.18%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.py
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.py
@@ -1,19 +1,29 @@
 class Solution:
     def minCost(self, nums: List[int], cost: List[int]) -> int:
-        def calc(l):
-            sm = 0
-            for i,x in enumerate(nums):
-                sm +=abs(l-x)*cost[i]
-            return sm 
-        l = min(nums)
-        r = max(nums) + 1
-        mid = (l+r)//2
+        size = len(nums)
+        nums_costs = sorted([(nums[i], cost[i]) for i in range(size)], key=itemgetter(0))
+        #nums_costs.item = (num, cost) sorted by num low to high
         
-        while l<r:
-            if calc(mid)<calc(mid+1):
-                r = mid
+        left_index = 0
+        left_cost_accrued = 0
+        left_per_unit_cost = nums_costs[left_index][1]
+        right_index = size - 1
+        right_cost_accrued = 0
+        right_per_unit_cost = nums_costs[right_index][1]
+
+        while right_index - left_index > 0:
+            if left_per_unit_cost < right_per_unit_cost:
+                low_value = nums_costs[left_index][0]
+                next_lowest_value = nums_costs[left_index + 1][0]
+                left_cost_accrued += (next_lowest_value - low_value) * left_per_unit_cost
+                left_per_unit_cost += nums_costs[left_index + 1][1]
+                left_index += 1
             else:
-                l = mid+1
-            mid = (l+r)//2
+                high_value = nums_costs[right_index][0]
+                next_highest_value = nums_costs[right_index - 1][0]
+                right_cost_accrued += (high_value - next_highest_value) * right_per_unit_cost
+                right_per_unit_cost += nums_costs[right_index - 1][1]
+                right_index -= 1
         
-        return calc(l)
+        total_cost = left_cost_accrued + right_cost_accrued
+        return total_cost
