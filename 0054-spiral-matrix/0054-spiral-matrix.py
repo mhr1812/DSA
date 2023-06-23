bf4f74961df0b8d9543114a02305f3ff874389de
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-06-23 19:11:38 +0530

    Time: 39 ms (90.95%), Space: 16.3 MB (81.70%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -2,62 +2,29 @@ class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         m = len(matrix)
         n = len(matrix[0])
+        ans = []
+        left,right = 0,n-1
+        top,bottom = 0,m-1
         
-        i = j = 0
+        while left<=right and top<=bottom:
+            for i in range(left,right+1):
+                ans.append(matrix[top][i])
+            top+=1
+            
+            for i in range(top,bottom+1):
+                ans.append(matrix[i][right])
+            right-=1
+            
+            if len(ans)==m*n:
+                break
+            
+            for i in range(right,left-1,-1):
+                ans.append(matrix[bottom][i])
+            bottom-=1
+            
+            for i in range(bottom,top-1,-1):
+                ans.append(matrix[i][left])
+            left+=1 
         
-        right = True
-        left = False
-        down = False
-        up = False
+        return ans 
         
-        res = []
-        cnt = 0
-        
-        while cnt < m*n:
-            
-            ##STOP RIGHT GO DOWN
-            if right and (j == n-1):
-                right = False
-                down = True
-            elif right and matrix[i][j+1] == None:
-                right = False
-                down = True
-                
-            ##STOP DOWN GO LEFT    
-            elif down and (i == m-1):
-                down = False
-                left = True
-            elif down and matrix[i+1][j] == None:
-                down = False
-                left = True
-                
-            ##STOP LEFT GO UP    
-            elif left and (j == 0):
-                left = False
-                up = True
-            elif left and matrix[i][j-1] == None:
-                left = False
-                up = True
-                
-            #STOP UP GO RIGHT    
-            elif up and (i == 0):
-                up = False
-                right = True
-            elif up and matrix[i-1][j] == None:
-                up = False
-                right = True    
-            
-            #store the result
-            res.append(matrix[i][j])
-            cnt += 1
-            
-            #Block the place
-            matrix[i][j] = None
-            
-            #NEXT MOVES
-            if right: j += 1
-            elif left: j -= 1
-            elif down : i += 1
-            elif up: i -= 1
-            
-        return res
