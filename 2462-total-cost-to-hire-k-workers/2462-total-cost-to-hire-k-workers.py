b0dc65fa20133195801a6119fe1ba6f3f4089cf2
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-06-26 19:55:36 +0530

    Time: 920 ms (40.00%), Space: 26.8 MB (98.66%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.py
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.py
@@ -1,19 +1,17 @@
 class Solution:
     def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
-        i = 0
-        j = len(costs) - 1
-        pq1 = []
-        pq2 = []
-
-        ans = 0
-        while k > 0:
-            while len(pq1) < candidates and i <= j:
-                heapq.heappush(pq1, costs[i])
-                i += 1
-            while len(pq2) < candidates and i <= j:
-                heapq.heappush(pq2, costs[j])
-                j -= 1
-
+        pq1,pq2 = [],[]
+        i,j,ans = 0,len(costs)-1,0
+        
+        for _ in range(k):
+            while len(pq1)<candidates and i<=j:
+                heapq.heappush(pq1,costs[i])
+                i+=1
+            
+            while len(pq2)<candidates and i<=j:
+                heapq.heappush(pq2,costs[j])
+                j-=1
+                
             t1 = pq1[0] if pq1 else float('inf')
             t2 = pq2[0] if pq2 else float('inf')
 
@@ -23,6 +21,5 @@ class Solution:
             else:
                 ans += t2
                 heapq.heappop(pq2)
-
-            k -= 1
+                
         return ans
