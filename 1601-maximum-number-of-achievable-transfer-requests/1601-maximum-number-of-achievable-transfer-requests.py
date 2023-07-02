46679e4d856c9a5ca9979b06191559bf208fdb3b
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-07-02 09:08:16 +0530

    Time: 99 ms (96.12%), Space: 16.5 MB (33.98%) - LeetHub

--- 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.py
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.py
@@ -1,12 +1,30 @@
 class Solution:
-    def maximumRequests(self, n: int, requests: List[List[int]]) -> int:
-        m = len(requests)
-        for ans in range(m,0,-1):
-            for c in combinations(range(m),ans):
-                deg = [0 for i in range(n)]
-                for i in c:
-                    deg[requests[i][0]]-=1
-                    deg[requests[i][1]]+=1
-                if not any(deg):
-                    return ans
-        return 0
+    def maximumRequests(self, n: int, req: List[List[int]]) -> int:
+        def canJoin(path, e): return req[e][0] == req[path[-1]][1]
+        def cyclic(path): return req[path[0]][0] == req[path[-1]][1]
+        def values(cycles): return len(sum(cycles, []))
+        def overlap(p1, p2): return any([True for e in p1 if e in p2])
+
+        def include(cycle, cycles): return [
+            c for c in cycles if not overlap(c, cycle)] + [cycle]
+
+        iter = range(len(req))
+
+        cycles = [[e] for e in iter if req[e][0] == req[e][1]]
+        paths = [[e] for e in iter]
+
+        newPaths = []
+        for i in range(2, n+1):
+            for path in paths:
+                for e in [e for e in iter if e not in path and canJoin(path, e)]:
+                    newPath = path + [e]
+                    if cyclic(newPath):
+                        newCycle = include(newPath, cycles)
+                        if values(newCycle) > values(cycles):
+                            cycles = newCycle
+                    else:
+                        newPaths.append(newPath)
+            paths = newPaths
+            newPaths = []
+
+        return values(cycles)
