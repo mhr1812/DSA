979c1fa75a6b21671ec3ba5ae7a9599767d0eef7
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-07-07 10:17:39 +0530

    Time: 184 ms (100.00%), Space: 16.6 MB (99.09%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
@@ -1,13 +1,14 @@
 class Solution:
-    def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
-        prev = ans = 0
-        n = len(answerKey)
-        d = defaultdict(int)
-        for i in range(n):
-            d[answerKey[i]]+=1
-            ans = max(ans,d[answerKey[i]])
-            if i-prev+1>ans+k:
-                d[answerKey[prev]]-=1
-                prev+=1
-        return n-prev
+    def maxConsecutiveAnswers(self, answerKey: str, target: int) -> int:
+        mx=0
+        for ch in "TF":
+            j=0
+            k=target
+            for i in answerKey:
+                if i==ch:k-=1
+                if k<0:
+                    if answerKey[j]==ch:k+=1
+                    j+=1
+            mx=max(mx,len(answerKey)-j)
+        return mx
                 
