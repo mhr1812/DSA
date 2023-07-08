5029bc220066227fec65f9eeb7c5c38caf2baf28
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-07-08 09:04:01 +0530

    Time: 702 ms (95.57%), Space: 30.1 MB (45.13%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.py
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.py
@@ -1,9 +1,10 @@
 class Solution:
     def putMarbles(self, weights: List[int], k: int) -> int:
         n = len(weights)
-        a = []
-        for i in range(n-1):
-            a.append(weights[i]+weights[i+1])
+        a = [x+y for x,y in pairwise(weights)]
+        if k==1:
+            return 0
         a.sort()
-        return sum(a[n-k:])-sum(a[:k-1])
+        k-=1
+        return sum(a[-k:])-sum(a[:k])
         
