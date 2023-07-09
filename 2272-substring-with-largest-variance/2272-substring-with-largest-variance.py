7dab8298e32435f147c0650b24edfec6aa1044bd
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-07-09 12:37:45 +0530

    Time: 2852 ms (90.60%), Space: 16.5 MB (54.70%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.py
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.py
@@ -1,21 +1,26 @@
 class Solution:
     def largestVariance(self, s: str) -> int:
-        st = set(s)
-        pairs = [(e1,e2) for e1 in st for e2 in st if e1!=e2]
-        ans=c1=c2= 0
-        for _ in range(2):
-            for pair in pairs:
-                c1=c2=0
-                for e in s:
-                    if e not in pair:
-                        continue
-                    if e==pair[0]:
-                        c1+=1
-                    else:
-                        c2+=1
-                    if c1<c2:
-                        c1=c2=0 
-                    elif c1>0 and c2>0:
-                        ans = max(ans,c1-c2)
-            s = s[::-1]
-        return ans
+        counter = collections.Counter(s)
+        res = 0
+        for a, b in itertools.permutations(counter, 2):
+            has_a, has_b = False, False
+            a_left, b_left = counter[a], counter[b]
+            total = 0
+            for ch in s:
+                if ch != a and ch != b:
+                    continue
+                if total < 0 and a_left and b_left:
+                    total = 0
+                    has_a, has_b = False, False
+                if ch == a:
+                    has_a = True
+                    a_left -= 1
+                    total += 1
+                if ch == b:
+                    has_b = True
+                    b_left -= 1
+                    total -= 1
+                if has_a and has_b:
+                    res = max(res, total)
+
+        return res
