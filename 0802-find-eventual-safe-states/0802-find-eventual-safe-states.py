af04f3b439cd1f98d5c7ae27dd0c8097fe9674c5
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-07-12 09:26:13 +0530

    Time: 645 ms (90.30%), Space: 24.4 MB (23.46%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,32 +1,29 @@
 class Solution:
-    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        n = len(graph)
-        visited = [False] * n
-        unsafe = [0] * n
-        
-        def dfs(i):
-            isSafe = True
-            for neighbor in graph[i]:
-                if visited[neighbor] or unsafe[neighbor] == 2:
-                    isSafe = False
-                    break
-                if unsafe[neighbor] == 1:
-                    continue
-                visited[neighbor] = True
-                if not dfs(neighbor):
-                    isSafe = False
-                visited[neighbor] = False
-            unsafe[i] = 1 if isSafe else 2
-            return isSafe
-        
-        for i in range(n):
-            if unsafe[i] == 0:
-                visited[i] = True
-                dfs(i)
-                visited[i] = False
+    
+    def dfs(self,graph,vis,dfs_vis,cycle_ele,s):
+        vis[s]=True
+        dfs_vis[s]=True
+        for node in graph[s]:
+            if (vis[node] == False) :
+                if (self.dfs(graph,vis,dfs_vis,cycle_ele,node)) :
+                    cycle_ele[s] = True
+                    return cycle_ele
+            elif (vis[node] and dfs_vis[node]):
+                cycle_ele[s]= True
+                return cycle_ele
+        dfs_vis[s] = False
+        return False
 
-        result = []
-        for i in range(len(unsafe)):
-            if unsafe[i] == 1:
-                result.append(i)
-        return result
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        ans=[]
+        l=len(graph)
+        vis=[False]*l
+        dfs_vis=[False]*l
+        cycle_ele=[False]*l
+        for i in range(l):
+            if (vis[i] == False) :
+                self.dfs(graph,vis,dfs_vis,cycle_ele,i)
+        for i in range(len(cycle_ele)):
+            if cycle_ele[i]== False  :
+                ans.append(i)
+        return ans
