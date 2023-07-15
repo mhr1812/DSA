c8ad844f62f7d1d867dd7f0be119ab3b4b59eeaf
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-07-15 12:09:59 +0530

    Time: 1083 ms (44.45%), Space: 61.2 MB (95.56%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
@@ -1,76 +1,12 @@
-from typing import List
-import heapq
-
-class Schedule:
-
-    def __init__(self, value=0, events=None):
-        self.value = value
-        self.events = [] if events is None else events
-
-    def __repr__(self):
-        return f"Schedule(value={self.value}, events={self.events})"
-    
-    def __lt__(self, other):
-        # less-than: smaller value, or more events to achieve the same value
-        return self.value < other.value or self.value == other.value and len(self.events) > len(other.events)
-
-    def add_event(self, value, k):
-
-        assert 1 <= k and len(self.events) <= k
-        
-        new_events = self.events.copy()
-        new_value = self.value
-
-        if k > len(new_events):
-            new_events.append(value)
-            new_value += value
-            if k == len(new_events):
-                heapq.heapify(new_events)
-        elif value > new_events[0]:
-            new_value += value - new_events[0]
-            heapq.heapreplace(new_events, value)
-
-        return Schedule(new_value, new_events)
-
 class Solution:
-    def __init__(self, debug=False):
-        self.debug = debug
-        
     def maxValue(self, events: List[List[int]], k: int) -> int:
-
-        if k == 1:
-            return max(e[2] for e in events)
-                
-        events = sorted(tuple(event) for event in events)
-        
-        best = Schedule()
-
-        schedules = []
-        for start, end, value in events:
-
-            okay = False
-            while schedules and schedules[0][0] < start:
-                e, s = heapq.heappop(schedules)
-                if self.debug:
-                    print(f"finish event at t={e:3d}, schedule={s}")
-                heapq.heappush(schedules, (end, s.add_event(value, k)))
-                if best < s:
-                    best = s
-                    okay = True
-
-            if self.debug:
-                print(f"start  event at t={start:3d}, {value=:3d}")
-
-            if not okay:
-                heapq.heappush(schedules, (end, best.add_event(value, k)))
-
-        for e, s in schedules:
-            if self.debug:
-                print(f"finish event at t={e:3d}, schedule={s}")
-            if best < s:
-                best = s
-
-        if self.debug:
-            print(f"best schedule overall: {best}")
-
-        return best.value
+        events.sort(key=lambda x: x[1])
+        events.insert(0, [events[0][0], events[0][1], -sys.maxsize])
+        res, sz, end_time = 0, len(events), [0]
+        t = [[0 for _ in range(k+1)] for _ in range(sz)]
+        for i in range(1, sz):
+            idx = bisect_left(end_time, events[i][0])
+            for j in range(1, k+1):
+                t[i][j] = max(t[i-1][j], t[idx-1][j-1] + events[i][2])
+            end_time.append(events[i][1])
+        return max(t[sz-1])
