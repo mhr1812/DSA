eeb1a4b0fa4821a9f9b1de9a564806afaab1a246
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-07-16 11:47:35 +0530

    Time: 48 ms (96.84%), Space: 16.5 MB (86.71%) - LeetHub

--- 1125-smallest-sufficient-team/1125-smallest-sufficient-team.py
+++ 1125-smallest-sufficient-team/1125-smallest-sufficient-team.py
@@ -1,37 +1,20 @@
 class Solution:
     def smallestSufficientTeam(self, req_skills: List[str], people: List[List[str]]) -> List[int]:
-        n = len(req_skills)
-        m = len(people)
-        skillMap = {skill: i for i, skill in enumerate(req_skills)}
-        dp = [None] * (1 << n)
-        dp[0] = []
+        skill_possessor_lookup = [set() for _ in req_skills]
+        skill_ID_lookup = {skill: pos for pos, skill in enumerate(req_skills)}
 
-        skillPerson = []
-        for i in range(m):
-            val = 0
-            for skill in people[i]:
-                val |= 1 << skillMap[skill]
-            skillPerson.append(val)
+        for person_id, person in enumerate(people):
+            for skill in person:
+                skill_ID = skill_ID_lookup[skill]
+                skill_possessor_lookup[skill_ID].add(person_id)
+        
+        queue = deque([(skill_possessor_lookup, [])])
 
-        banned = [False] * m
-        for i in range(m):
-            for j in range(i + 1, m):
-                val = skillPerson[i] | skillPerson[j]
-                if val == skillPerson[i]:
-                    banned[j] = True
-                elif val == skillPerson[j]:
-                    banned[i] = True
-
-        for i in range(m):
-            if banned[i]:
-                continue
-            curSkill = skillPerson[i]
-
-            for j in range(len(dp)):
-                if dp[j] is None:
-                    continue
-                comb = j | curSkill
-                if dp[comb] is None or len(dp[j]) + 1 < len(dp[comb]):
-                    dp[comb] = dp[j] + [i]
-
-        return dp[(1 << n) - 1]
+        while queue:
+            top_skills, curr_group = queue.popleft()
+            rarest_skill_possessors = min(top_skills, key=len)
+            for person_id in rarest_skill_possessors:
+                remaining_skills = [possessor for possessor in top_skills if person_id not in possessor]
+                if not remaining_skills:
+                    return curr_group + [person_id]
+                queue.append((remaining_skills, curr_group + [person_id]))
