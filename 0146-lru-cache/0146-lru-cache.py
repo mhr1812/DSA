3871bd5d4e623ccf2803aa9873f1701a3c3581de
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-07-18 08:31:46 +0530

    Time: 706 ms (89.45%), Space: 77.5 MB (86.83%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,69 +1,22 @@
+from collections import OrderedDict
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.LRU = {}
         self.capacity = capacity
-        self.head = None
-        self.tail = None
+        self.dic = OrderedDict()
 
     def get(self, key: int) -> int:
-        if key in self.LRU:
-            value, prev, next = self.LRU[key]
-
-            if next is not None:
-                self.LRU[next][1] = prev
-                if prev is not None: 
-                    self.LRU[prev][2] = next
-                elif prev is None: 
-                    self.head = next
-
-                self.LRU[key] = [value, self.tail, None]
-                if self.tail is not None:
-                    self.LRU[self.tail][2] = key
-
-            self.tail = key
-            return value
-
-        else:
+        if key not in self.dic:
             return -1
+        
+        self.dic.move_to_end(key)
+        return self.dic[key]
 
     def put(self, key: int, value: int) -> None:
-        if key in self.LRU:
-            _, prev, next = self.LRU[key]
-
-            if next is None:
-                self.LRU[key][0] = value
-            else:
-                self.LRU[next][1] = prev
-                if prev is not None: 
-                    self.LRU[prev][2] = next
-                elif prev is None: 
-                    self.head = next
-
-                self.LRU[key] = [value, self.tail, None]
-                if self.tail is not None:
-                    self.LRU[self.tail][2] = key
-
-        elif len(self.LRU) == self.capacity:
-                next = self.LRU[self.head][2]
-                del self.LRU[self.head]
-                if next is not None:
-                    self.head = next
-                    self.LRU[self.head][1] = None
-
-                    self.LRU[key] = [value, self.tail, None]
-                    if self.tail is not None:
-                        self.LRU[self.tail][2] = key
-                elif next is None: 
-                    self.head = key
-                    self.LRU[key] = [value, None, None]
-
-        else:
-            if self.head is None:
-                self.head = key
-
-            self.LRU[key] = [value, self.tail, None]
-            if self.tail is not None:
-                self.LRU[self.tail][2] = key
-
-        self.tail = key
+        if key in self.dic:
+            self.dic.move_to_end(key)
+        
+        self.dic[key] = value
+        if len(self.dic) > self.capacity:
+            self.dic.popitem(False)
