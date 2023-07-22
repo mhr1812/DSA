8711db2869e3e85b44d3562cbc6c54f638a9575f
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-07-22 22:09:55 +0530

    Time: 106 ms (99.76%), Space: 18.7 MB (67.80%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.py
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.py
@@ -1,21 +1,57 @@
 class Solution:
-    def dp(self,x,y,n,visited,k):
-        if k<0:
-            return 1
-        if (x<0 or x>=n) or (y<0 or y>=n):
-            return 0
-        if (x,y,k) in visited:
-            return visited[(x,y,k)]
-        a=self.dp(x+2,y+1,n,visited,k-1)*1/8
-        b=self.dp(x+2,y-1,n,visited,k-1)*1/8
-        c=self.dp(x-2,y+1,n,visited,k-1)*1/8
-        d=self.dp(x-2,y-1,n,visited,k-1)*1/8
-        e=self.dp(x+1,y+2,n,visited,k-1)*1/8
-        f=self.dp(x+1,y-2,n,visited,k-1)*1/8
-        g=self.dp(x-1,y+2,n,visited,k-1)*1/8
-        h=self.dp(x-1,y-2,n,visited,k-1)*1/8
-        visited[(x,y,k)]=a+b+c+d+e+f+g+h
-        return a+b+c+d+e+f+g+h
-    
     def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
-        return self.dp(row,column,n,{},k)
+
+        tNodes = 0
+        cache = []
+        for i in range(k):
+            cache.append([[-1] * n for _ in range(n)])
+        
+        moves = [ (2,1),(2,-1),(-2,1),(-2,-1), (1,2),(1,-2),(-1,2),(-1,-2) ]
+
+        def inRange(t):
+            return t>=0 and t<n
+
+        def dfs(curK, r, c):
+            if curK==k:
+                #nonlocal tNodes
+                #tNodes += 1
+                return 1
+
+            #curK += 1
+            tmp = 0
+
+            for rMove, cMove in moves:
+                rp = r + rMove
+                cp = c + cMove
+
+                if inRange(rp) and inRange(cp):
+
+                    #see if result is already cached
+                    if cache[curK][rp][cp]==-1:
+                        z = dfs(curK+1, rp, cp)
+
+                        #add z in cache
+                        for x, y in [(rp, cp), (cp, rp)]:
+                            cache[curK][x][y] = z
+
+                            #vertical image
+                            cache[curK][x][n-1-y] = z
+
+                            #horizontal image
+                            cache[curK][n-1-x][y] = z
+
+                            #other diagonal image
+                            cache[curK][n-1-y][n-1-x] = z
+
+                    else:
+                        z = cache[curK][rp][cp]
+
+                    tmp += z
+
+            return tmp
+
+        tNodes = dfs(0, row, column)
+        
+        print(tNodes)
+        res = tNodes * pow(0.125, k)
+        return res
