53af3ffecd5e84d93a43c0f52b163ae91442b3a0
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-07-25 17:02:06 +0530

    Time: 605 ms (37.41%), Space: 30.2 MB (55.46%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -1,15 +1,17 @@
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
         left = 0
-        right = len(arr) - 1
-        while(left < right):
-            middle = (left + right)//2
+        right = len(arr) -1
+        while left <= right:
+            mid = (right + left) // 2
 
-            if arr[middle - 1] < arr[middle] and arr[middle + 1] < arr[middle]:
-                return middle
-            
-            if arr[middle + 1] > arr[middle] and arr[middle -1] < arr[middle]:
-                left = middle
-            
-            if arr[middle + 1] < arr[middle] and arr[middle] < arr[middle -1]:
-                right = middle
+            if arr[mid-1] < arr[mid] > arr[mid+1]:
+                return mid 
+            elif arr[mid+1] > arr[mid]:
+                #+1 greater need to go to the right since larger arr there 
+                left = mid + 1
+            else:
+                #-1 greater need to go to the left since larger arr there
+                right = mid - 1
+        
+        return right
