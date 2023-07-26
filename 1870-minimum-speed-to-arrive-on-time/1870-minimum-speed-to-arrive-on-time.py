96ecb0e181b0b45520bc1db6679f78e37ddd269e
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-07-26 19:45:56 +0530

    Time: 1917 ms (99.15%), Space: 47.4 MB (9.40%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.py
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.py
@@ -1,35 +1,25 @@
+from numpy import array, ceil, sum
+
 class Solution:
     def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
-        
-        
-        if len(dist)-1 >= hour:
+
+        def check(speed: float) -> bool:
+            time = array(dist) / speed
+            time = ceil(time)
+            time[-1] = dist[-1] / speed
+            return sum(time) <= hour
+
+        # Check that the solution exists
+        if len(dist) > ceil(hour):
             return -1
-        
-        def helper(speed):
-            #print(nan)
-            
-            total_hour = 0
-            for i, d in enumerate(dist):
-                if i == len(dist)-1:
-                    time_taken = d/speed
-                else:
-                    time_taken = ceil(d/speed)
-                total_hour += time_taken
-            #print(speed, total_hour)
-            
-            return total_hour <= hour
-        
-        
-        l = 1
-        #r = max(dist)
-        r = 10**9
-        
-        while l < r:
-            m = l + (r-l) // 2
-            #print(l,r,m)
-            if helper(m):
-                r = m
+
+        # Binary search
+        unfeasible, solution = 0, 10**7
+        while solution - unfeasible > 1:
+            mid = (unfeasible + solution) // 2
+            if check(mid):
+                solution = mid
             else:
-                l = m + 1
-        
-        return l
+                unfeasible = mid
+
+        return solution
