b8aae52eae7c8726c9835aabbb723abc90d50541
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-07-30 13:21:54 +0530

    Time: 967 ms (50.97%), Space: 16.5 MB (66.45%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.py
+++ 0664-strange-printer/0664-strange-printer.py
@@ -1,37 +1,16 @@
 class Solution:
-	def strangePrinter(self, s: str) -> int:
-		
-		s = ''.join([c for i, c in enumerate(s)
-					 if i == 0 or c != s[i - 1]])
+    def strangePrinter(self, s: str) -> int:
+        n = len(s)
+        t = [[0] * n for _ in range(n)]
 
-		next_occ_search = {}
-		next_occurrence_of_letter = list(range(len(s)))
-		for i, c in enumerate(s):
-			if c in next_occ_search:
-				next_occurrence_of_letter[next_occ_search[c]] = i
-			next_occ_search[c] = i
+        for i in range(n - 1, -1, -1):
+            t[i][i] = 1
+            for j in range(i + 1, n):
+                if s[i] == s[j]:
+                    t[i][j] = t[i][j - 1]
+                else:
+                    t[i][j] = float('inf')
+                    for k in range(i, j):
+                        t[i][j] = min(t[i][j], t[i][k] + t[k + 1][j])
 
-		for v in next_occ_search.values():
-			next_occurrence_of_letter[v] = 10**10
-
-		@functools.lru_cache(None)
-		def dp(left_ind: int, right_ind: int) -> int:
-
-			if left_ind >= right_ind:
-				return 0
-
-			if s[right_ind] == s[left_ind]:
-				return dp(left_ind + 1, right_ind)
-
-			ans = 1 + dp(left_ind + 1, right_ind)
-
-			pivot_index = next_occurrence_of_letter[left_ind]
-			while pivot_index <= right_ind:
-				ans = min(ans, 1 + dp(left_ind + 1, pivot_index)
-						  + dp(pivot_index + 1, right_ind))
-				pivot_index = next_occurrence_of_letter[pivot_index]
-
-			return ans
-
-		s += '#'
-		return dp(0, len(s) - 1)
+        return t[0][n - 1]
