d9ca36617847b1546c2a3252162baa9e2ce573db
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-07-31 15:04:14 +0530

    Time: 126 ms (100.00%), Space: 16.2 MB (98.86%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
@@ -1,18 +1,12 @@
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
-        n=len(s1)
-        m=len(s2)
-        dp=[[0 for x in range(m+1)] for x in range(n+1)]
-        for i in range(1,n+1):
-            dp[i][0]=dp[i-1][0]+ord(s1[i-1])
-
-        for i in range(1,m+1):
-            dp[0][i]=dp[0][i-1]+ord(s2[i-1])
-        for i in range(1,n+1):
-            for j in range(1,m+1):
-                if s1[i-1]==s2[j-1]:
-                    dp[i][j]=dp[i-1][j-1]
-                else:
-                    dp[i][j]=min(dp[i-1][j]+ord(s1[i-1]),dp[i][j-1]+ord(s2[j-1]))
-
-        return dp[n][m] 
+        dp = [0] * len(s2)
+        for c1 in s1:
+            prev = 0
+            for i, c2 in enumerate(s2):
+                last = dp[i]
+                if c1 == c2:
+                    dp[i] = prev + ord(c1)
+                if prev <= last:
+                    prev = last
+        return sum(map(ord, f'{s1}{s2}')) - 2 * max(dp)
