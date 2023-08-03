38e7a2f71c0ec124e03101d16d685ca8fd8b848f
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-08-03 19:55:16 +0530

    Time: 26 ms (99.88%), Space: 16.5 MB (19.66%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,26 +1,24 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if digits=="":
+        # base case: no digits
+        if not digits:
             return []
-        d = {
-            '2': 'abc',
-            '3': 'def',
-            '4': 'ghi',
-            '5': 'jkl',
-            '6': 'mno',
-            '7': 'pqrs',
-            '8': 'tuv',
-            '9': 'wxyz',
-        }
-        ans = []
-        def recur(idx, comb):
-            if idx == len(digits):
-                ans.append(comb)
-                return
-            
-            for letter in d[digits[idx]]:
-                recur(idx + 1, comb + letter)
-        
-        recur(0, "")
 
-        return ans
+        # create hashmap (dict) to store phone digit (key) and corresponding letters (value)
+        phone = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+        res = []
+
+        # helper function backtrack to do backtracking
+        def backtrack(combination, next_digits):
+            # if no more digits to process, append current combination to res list and return
+            if not next_digits:
+                res.append(combination)
+                return
+
+            # otherwise iterate through each letter that the first remaining digit maps to
+            # recursively call backtrack with new combination & remaining digits
+            for letter in phone[next_digits[0]]:
+                backtrack(combination + letter, next_digits[1:])
+
+        backtrack("", digits)
+        return res
