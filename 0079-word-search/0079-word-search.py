365c0610cc91f3a5d8ef8d9d276b9ca6a840dd57
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-08-05 13:24:24 +0530

    Time: 6998 ms (60.58%), Space: 16.3 MB (73.40%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,42 +1,58 @@
-class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        
-        R = len(board)
-        C = len(board[0])
-        
-        if len(word) > R*C:
-            return False
-        
-        count = Counter(sum(board, []))
-        
-        for c, countWord in Counter(word).items():
-            if count[c] < countWord:
-                return False
-            
-        if count[word[0]] > count[word[-1]]:
-             word = word[::-1]
-                        
-        seen = set()
-        
-        def dfs(r, c, i):
-            if i == len(word):
-                return True
-            if r < 0 or c < 0 or r >= R or c >= C or word[i] != board[r][c] or (r,c) in seen:
-                return False
-            
-            seen.add((r,c))
-            res = (
-                dfs(r+1,c,i+1) or 
-                dfs(r-1,c,i+1) or
-                dfs(r,c+1,i+1) or
-                dfs(r,c-1,i+1) 
-            )
-            seen.remove((r,c))  #backtracking
+POSITION_MODIFIERS = ((0, 1), (1, 0), (-1, 0), (0, -1))
 
-            return res
+class Solution:
+    def _exist(self, board, word, word_pos, i, j, visited) -> bool:
+        if word_pos == len(word):
+            return True
+
+        if (i, j) in visited:
+            return False
+
+        if i >= len(board) or i < 0:
+            return False
+
+        if j >= len(board[0]) or j < 0:
+            return False
+
+        if board[i][j] != word[word_pos]:
+            return False
+
+        visited.add(
+            (i, j)
+        )
+
+        for mod_i, mod_j in POSITION_MODIFIERS:
+            updated_i = i + mod_i
+            updated_j = j + mod_j
+
+            if self._exist(
+                board, word, word_pos+1, updated_i, updated_j, visited
+            ):
+                return True
+
+        visited.remove(
+            (i, j)
+        )
+
+        return False
+
+    def _exist_starting_from_cell(
+        self, 
+        board: List[List[str]], 
+        word: str,
+        i: int,
+        j: int
+    ) -> bool:
+        return self._exist(
+            board, word, word_pos=0, i=i, j=j, visited=set()
+        )
         
-        for i in range(R):
-            for j in range(C):
-                if dfs(i,j,0):
+
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        m = len(board)
+        n = len(board[0])
+        for i in range(m):
+            for j in range(n):
+                if self._exist_starting_from_cell(board, word, i, j):
                     return True
         return False
