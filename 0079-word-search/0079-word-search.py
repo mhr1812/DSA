196b74cfbf58bd0349cb0de535b1805ec91b6c66
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-08-05 13:23:45 +0530

    Time: 41 ms (99.80%), Space: 16.5 MB (40.07%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,22 +1,42 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        if not board:
-            return False
-        m,n = len(board),len(board[0])
         
-        def dfs(i,j,word):
-            if not word:
-                return True 
-            if i<0 or i>=m or j<0 or j>=n or word[0]!=board[i][j]:
+        R = len(board)
+        C = len(board[0])
+        
+        if len(word) > R*C:
+            return False
+        
+        count = Counter(sum(board, []))
+        
+        for c, countWord in Counter(word).items():
+            if count[c] < countWord:
                 return False
-            t = board[i][j]
-            board[i][j] = 'v'
-            ans = dfs(i+1,j,word[1:]) or dfs(i-1,j,word[1:]) or dfs(i,j+1,word[1:]) or dfs(i,j-1,word[1:])
-            board[i][j] = t
-            return ans
             
-        for i in range(m):
-            for j in range(n):
-                if dfs(i,j,word):
-                    return True 
+        if count[word[0]] > count[word[-1]]:
+             word = word[::-1]
+                        
+        seen = set()
+        
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+            if r < 0 or c < 0 or r >= R or c >= C or word[i] != board[r][c] or (r,c) in seen:
+                return False
+            
+            seen.add((r,c))
+            res = (
+                dfs(r+1,c,i+1) or 
+                dfs(r-1,c,i+1) or
+                dfs(r,c+1,i+1) or
+                dfs(r,c-1,i+1) 
+            )
+            seen.remove((r,c))  #backtracking
+
+            return res
+        
+        for i in range(R):
+            for j in range(C):
+                if dfs(i,j,0):
+                    return True
         return False
