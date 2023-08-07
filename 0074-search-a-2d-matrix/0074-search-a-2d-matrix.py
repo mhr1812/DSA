73f0a5d49c1abe965a9f46bf5f0831f45ea3e009
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-08-07 19:12:24 +0530

    Time: 57 ms (64.62%), Space: 17 MB (28.37%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,18 +1,31 @@
 class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:        
-        rows, cols = len(matrix), len(matrix[0])
-        left, right = 0, rows * cols - 1
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        verticalHigh = len(matrix)-1
+        verticalLow = 0
 
-        while left <= right:
-            mid = (left + right) // 2
-            row, col = mid // cols, mid % cols
-            guess = matrix[row][col]
 
-            if guess == target:
-                return True
-            elif guess < target:
-                left = mid + 1
+        horizontalHigh = len(matrix[0])-1
+        horizontalLow = 0
+
+
+        while verticalLow<=verticalHigh:
+            
+            verticalMid = (verticalHigh+verticalLow)//2
+            if target > matrix[verticalMid][horizontalHigh]:
+                verticalLow = verticalMid +1
+            elif target < matrix[verticalMid][horizontalLow]:
+                verticalHigh = verticalMid -1
             else:
-                right = mid - 1
-
+                break
+        searchRow = verticalMid
+        
+        while horizontalLow<=horizontalHigh:
+            horizontalMid = (horizontalHigh + horizontalLow)//2
+            print(matrix[searchRow][horizontalMid])
+            if matrix[searchRow][horizontalMid] == target:
+                return True
+            elif matrix[searchRow][horizontalMid] < target:
+                horizontalLow = horizontalMid + 1
+            elif  matrix[searchRow][horizontalMid] > target:
+                horizontalHigh = horizontalMid -1
         return False
