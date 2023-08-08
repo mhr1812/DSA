d890584ce694107993d0ecedf0162cfb685a80a7
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-08-08 23:37:51 +0530

    Time: 47 ms (89.04%), Space: 16.6 MB (94.24%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,21 +1,22 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        ans = -1
-        l,r = 0,len(nums)-1
-        while l<=r:
-            mid = (l+r)//2
-            if nums[mid]==target:
-                ans = mid
-                return ans
-            elif nums[mid]>=nums[l]:
-                if nums[l]<=target<=nums[mid]:
-                    r = mid-1
-                else:
-                    l = mid+1
-            elif nums[mid]<=nums[r]:
-                if nums[mid]<=target<=nums[r]:
-                    l = mid+1
-                else:
-                    r = mid-1
-        return ans
-        
+        low = 0
+        high = len(nums) - 1
+
+        while (low <= high) :
+            mid = (low + high)//2
+            if nums[mid] == target :
+                return mid
+            
+            if nums[low] <= nums[mid] :
+                if target >= nums[low] and target <= nums[mid]:
+                    high = mid - 1
+                else :
+                    low = mid + 1
+            else :
+                if target >= nums[mid] and target <= nums[high] :
+                    low = mid + 1
+                else :
+                    high = mid - 1
+            
+        return -1
