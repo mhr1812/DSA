9c68e7bf93572a5478dd8ff70c421e963ae3c4c7
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-08-23 20:08:53 +0530

    Time: 42 ms (75.02%), Space: 16.1 MB (96.85%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,19 +1,22 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        d = defaultdict(int)
-        for c in s:
-            d[c]+=1
-        arr = [(-v,k) for k,v in d.items()]
-        heapq.heapify(arr)
         ans = []
-        pf, pc = 0,""
-        while arr:
-            freq,char = heapq.heappop(arr)
-            ans.append(char)
-            if pf<0:
-                heapq.heappush(arr,(pf,pc))
-            freq+=1
-            pf,pc = freq,char
-        if len(ans)==len(s):
-            return "".join(ans)
-        return ""
+        pq = [(-count, char) for char, count in collections.Counter(s).items()]
+        heapify(pq)
+
+        while pq:
+            count, char = heappop(pq)
+            if not ans or char != ans[-1]:
+                ans.append(char)
+                if count + 1 != 0: 
+                    heapq.heappush(pq, (count + 1, char))
+            else:
+                if not pq: return ''
+                count_next, char_next = heappop(pq)
+                ans.append(char_next)
+                if count_next + 1 != 0:
+                    heapq.heappush(pq, (count_next + 1, char_next))
+                heapq.heappush(pq, (count, char))
+
+        return ''.join(ans)
+
