3ab8da8ff0a6f0f0db2d7697fcea724142a9ae04
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-09-03 21:25:05 +0530

    Time: 43 ms (47.29%), Space: 16.1 MB (95.35%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,11 +1,3 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        m -=1
-        n -=1
-        up = 1
-        for i in range(m+1, m+n+1):
-            up *= i
-        down = 1
-        for i in range(1, n+1):
-            down *= i
-        return up//down
+        return comb(m + n - 2, m - 1)
