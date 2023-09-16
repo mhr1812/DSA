1d8f00dd8f59229555d64646fe0f4cb35b69de0d
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-09-16 18:27:55 +0530

    Time: 453 ms (99.81%), Space: 17.6 MB (95.34%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,18 +1,27 @@
 class Solution:
-    def minimumEffortPath(self, hts: list[list[int]]) -> int:
-
-        m,n, dx,dy = len(hts),len(hts[0]), 1,0
-        ans, heap, unseen = 0, [(0, 0, 0)], set(product(range(m), range(n)))
-
-        while heap:
-            effort, x, y = heappop(heap)
-            unseen.discard((x, y))
-            ans = max(ans,effort)
-            if (x,y) == (m-1, n-1): break
-
-            for _ in range(4):
-                X, Y, dx, dy = x + dx, y + dy, -dy, dx
-                if (X,Y) in unseen:
-                    heappush(heap, (abs(hts[x][y] - hts[X][Y]), X, Y))
-
-        return ans
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        m, n = len(heights), len(heights[0])
+        res = 0
+        min_heap = [(0, 0, 0)] 
+        
+        while min_heap:
+            diff, x, y = heappop(min_heap)
+            res = max(res, diff)
+            
+            if x == m-1 and y == n-1:
+                return res
+            
+            if heights[x][y] == 0:
+                continue
+                
+            curr_height = heights[x][y]
+            heights[x][y] = 0
+            
+            for dx, dy in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+                new_x, new_y = x + dx, y + dy
+                
+                if 0 <= new_x < m and 0 <= new_y < n and heights[new_x][new_y] != 0:
+                    new_diff = abs(heights[new_x][new_y] - curr_height)
+                    heappush(min_heap, (new_diff, new_x, new_y))
+        
+        return res
