332b6e75944265540e307490654d4aaa6ef55abe
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-09-29 12:58:52 +0530

    Added solution - LeetHub

---
+++ Minimum Platforms - GFG/minimum-platforms.py
@@ -0,0 +1,38 @@
+#User function Template for python3
+
+class Solution:    
+    #Function to find the minimum number of platforms required at the
+    #railway station such that no train waits.
+    def minimumPlatform(self,n,arr,dep):
+        # code here
+        arr.sort(),dep.sort()
+        x,y = 1,0
+        c =ans= 1
+        while x<n and y<n:
+            if dep[y]>=arr[x]:
+                c+=1
+                x+=1
+            else:
+                c-=1
+                y+=1
+            ans = max(ans,c)
+        return ans
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n = int(input())
+        arrival = list(map(int, input().strip().split()))
+        departure = list(map(int, input().strip().split()))
+        ob=Solution()
+        print(ob.minimumPlatform(n,arrival,departure))
+# } Driver Code Ends
