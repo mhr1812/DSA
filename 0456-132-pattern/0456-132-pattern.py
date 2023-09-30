3fbeae457e7b381214200cac8f2ba5000fef3bed
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-09-30 13:08:08 +0530

    Time: 400 ms (65.83%), Space: 36.2 MB (94.24%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.py
+++ 0456-132-pattern/0456-132-pattern.py
@@ -1,30 +1,12 @@
-# class Solution:
-#     def find132pattern(self, nums: List[int]) -> bool:
-#         st = []
-#         curr = nums[0]
-#         for n in nums[1:]:
-#             while st and n>=st[-1][1]:
-#                 st.pop()
-#             if st and n>st[-1][1]:
-#                 return True
-#             st.append([n,curr])
-#             curr = min(curr,n)
-#         return False
-
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
-        
         st = []
-        cur_min = nums[0]
-
+        curr = nums[0]
         for n in nums[1:]:
-            while st and n >= st[-1][0]:
+            while st and n>=st[-1][0]:
                 st.pop()
-            
-            if st and n > st[-1][1]:
+            if st and n>st[-1][1]:
                 return True
-            
-            st.append([n, cur_min])
-            cur_min = min(cur_min, n)
-        
+            st.append([n,curr])
+            curr = min(curr,n)
         return False
