eebaaf4dfb91ff5a01b4b1e9c75a38a4be671fc3
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-10-02 19:00:32 +0530

    Time: 229 ms (35.14%), Space: 17.9 MB (7.65%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
@@ -1,9 +1,10 @@
 class Solution:
     def winnerOfGame(self, colors: str) -> bool:
-        c = defaultdict(int)
-        for x,t in groupby(colors):
-            c[x]+=max(len(list(t))-2,0)
-        if c['A']>c['B']:
+        c = collections.Counter()
+        for x, t in groupby(colors):
+            c[x] += max(len(list(t)) - 2, 0)
+
+        if c['A'] > c['B']:
             return True
         return False
         
