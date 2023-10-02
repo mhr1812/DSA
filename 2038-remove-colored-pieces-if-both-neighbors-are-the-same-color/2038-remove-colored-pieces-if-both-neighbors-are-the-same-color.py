cbd6ab78a090cc13eea7e51ee09c62e3fa44f897
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-10-02 19:01:40 +0530

    Time: 94 ms (98.26%), Space: 19.7 MB (5.23%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
@@ -1,10 +1,20 @@
 class Solution:
     def winnerOfGame(self, colors: str) -> bool:
-        c = collections.Counter()
-        for x, t in groupby(colors):
-            c[x] += max(len(list(t)) - 2, 0)
+        if len(colors) < 3:
+            return False
 
-        if c['A'] > c['B']:
-            return True
-        return False
-        
+        A_groups = colors.split("B")
+        B_groups = colors.split("A")
+
+        Alice_moves = 0
+        Bob_moves = 0
+
+        for A_group, B_group in zip(A_groups, B_groups):
+            if len(A_group) >= 3:
+                move = len(A_group) - 3 + 1
+                Alice_moves += move
+            if len(B_group) >= 3:
+                move = len(B_group) - 3 + 1
+                Bob_moves += move
+
+        return Alice_moves > Bob_moves
