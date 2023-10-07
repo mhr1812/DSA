e5eb159d4781c5dd1b2d4fe8b783637dd0bcb348
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-10-08 00:25:52 +0530

    Time: 81 ms (97.35%), Space: 16.4 MB (95.58%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
@@ -1,30 +1,12 @@
 class Solution:
     def numOfArrays(self, n: int, m: int, k: int) -> int:
-        dp = [[0] * (k + 1) for _ in range(m + 1)]
-        prefix = [[0] * (k + 1) for _ in range(m + 1)]
-        prevDp = [[0] * (k + 1) for _ in range(m + 1)]
-        prevPrefix = [[0] * (k + 1) for _ in range(m + 1)]
-        MOD = 10 ** 9 + 7
-        
-        for num in range(1, m + 1):
-            dp[num][1] = 1
+        if m < k: return 0
 
-        for i in range(1, n + 1):
-            if i > 1:
-                dp = [[0] * (k + 1) for _ in range(m + 1)]
-
-            prefix = [[0] * (k + 1) for _ in range(m + 1)]
-            for max_num in range(1, m + 1):
-                for cost in range(1, k + 1):                    
-                    ans = (max_num * prevDp[max_num][cost]) % MOD
-                    ans = (ans + prevPrefix[max_num - 1][cost - 1]) % MOD
-
-                    dp[max_num][cost] += ans
-                    dp[max_num][cost] %= MOD
-                    
-                    prefix[max_num][cost] = (prefix[max_num - 1][cost] + dp[max_num][cost]) % MOD
-                    
-            prevDp = dp
-            prevPrefix = prefix
-
-        return prefix[m][k]
+        dp = [[int(ki == 0)] * m for ki in range(k)]
+        for _ in range(n - 1):
+            for i in range(k - 1, -1, -1):
+                acc = 0
+                for j in range(m - 1, -1, -1):
+                    dp[i][j] = dp[i][j] * (j + 1) + acc
+                    if i > 0: acc += dp[i - 1][j]
+        return sum(dp[-1]) % (10 ** 9 + 7)
