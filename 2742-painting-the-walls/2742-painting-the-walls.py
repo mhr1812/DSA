ea7577ff9e83226015c32bded24bec2f88c43828
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-10-14 14:47:09 +0530

    Time: 933 ms (97.41%), Space: 16.3 MB (95.93%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.py
+++ 2742-painting-the-walls/2742-painting-the-walls.py
@@ -1,29 +1,12 @@
 class Solution:
     def paintWalls(self, cost: List[int], time: List[int]) -> int:
-        postfix_times = time.copy()
+        kMax = 500_000_000
+        n = len(cost)
+    # dp[i] := min cost to paint i walls by painters so far
+        dp = [0] + [kMax] * n
 
-        for i in range(len(postfix_times) - 2, -1, -1):
-            postfix_times[i] += postfix_times[i +1]
+        for c, t in zip(cost, time):
+            for walls in range(n, 0, -1):
+                dp[walls] = min(dp[walls], dp[max(walls - t - 1, 0)] + c)
 
-        visited = {}
-
-        def dp(index, time_painted):
-            if index == len(cost):
-                return 0 if time_painted >= 0 else float("inf")
-
-            if time_painted >= len(cost) - index:
-                return 0
-
-            if time_painted + postfix_times[index] < 0:
-                return float("inf")
-
-            if (index, time_painted) in visited:
-                return visited[(index, time_painted)]
-            
-            visited[(index, time_painted)] = min(
-                dp(index + 1, time_painted - 1),
-                cost[index] + dp(index + 1,
-                time_painted + time[index]))
-            return visited[(index, time_painted)]
-        
-        return dp(0, 0)
+        return dp[n]
