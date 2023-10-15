e71e8eaac66feb6cb60cd6d2e1042b6d84ad768c
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-10-15 15:50:37 +0530

    Time: 221 ms (85.55%), Space: 25.6 MB (61.11%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.py
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.py
@@ -1,14 +1,19 @@
-mod = 10**9+7
 class Solution:
     def numWays(self, steps: int, arrLen: int) -> int:
-        pos = min(steps//2+1,arrLen-1)
-        dp = [[0 for _ in range(pos+1)] for _ in range(steps+1)]
+        m = steps
+        n = min(steps // 2 + 1, arrLen)
+
+        dp = [[0] * n for _ in range(m + 1)]
+
         dp[0][0] = 1
-        for i in range(1,steps+1):
-            for j in range(pos+1):
-                dp[i][j] = dp[i-1][j]
-                if j-1>=0:
-                    dp[i][j] = (dp[i][j]+dp[i-1][j-1])%mod
-                if j+1<=pos:
-                    dp[i][j] = (dp[i][j]+dp[i-1][j+1])%mod
-        return dp[steps][0]
+        mod = 10 ** 9 + 7
+
+        for i in range(1, m + 1):
+            for j in range(n):
+                dp[i][j] = dp[i-1][j]  
+                if j > 0:
+                    dp[i][j] += dp[i-1][j-1]
+                if j < n - 1:
+                    dp[i][j] += dp[i-1][j+1]
+
+        return dp[m][0] % mod
