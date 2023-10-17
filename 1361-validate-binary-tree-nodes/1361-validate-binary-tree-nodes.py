93a527c062f1a2177e98ccd7f4f2266262835b25
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-10-17 19:12:28 +0530

    Time: 251 ms (97.01%), Space: 18.9 MB (70.36%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.py
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.py
@@ -1,33 +1,22 @@
 class Solution:
-    def validateBinaryTreeNodes(self, n, leftChild, rightChild):
-        graph = defaultdict(list)
-        in_degree = [0] * n
-
-        for node in range(n):
+    def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
+        childs_set = set(leftChild+rightChild)
+        root = 0
+        while root in childs_set and root < n:
+            root += 1
+        if root == n:
+            return False
+        queue = deque([root])
+        while queue :
+            node = queue.popleft()
             left, right = leftChild[node], rightChild[node]
+            if left not in childs_set or right not in childs_set:
+                return False
             if left != -1:
-                graph[node].append(left)
-                in_degree[left] += 1
+                queue.append(left)
+                childs_set.discard(left)
             if right != -1:
-                graph[node].append(right)
-                in_degree[right] += 1
-
-        root_candidates = [node for node in range(n) if in_degree[node] == 0]
-
-        if len(root_candidates) != 1:
-            return False  
-        root = root_candidates[0]
-
-        queue = [root]
-        seen = set([root])
-
-        while queue:
-            node = queue.pop(0)
-            if node in graph:
-                for child in graph[node]:
-                    if child in seen:
-                        return False  
-                    seen.add(child)
-                    queue.append(child)
-
-        return len(seen) == n
+                queue.append(right)
+                childs_set.discard(right)
+            n -= 1
+        return not n
