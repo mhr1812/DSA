192ee7b3ff27b12761200a7c74588b10166ed9fd
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-10-21 17:27:54 +0530

    Time: 1345 ms (70.26%), Space: 30.1 MB (53.02%) - LeetHub

--- 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.py
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.py
@@ -1,20 +1,13 @@
 class Solution:
     def constrainedSubsetSum(self, nums: List[int], k: int) -> int:
-      
-        q = deque()
-        res = float('-inf')
-
-        for i, num in enumerate(nums):
-            total = num + q[0][1] if q else num
-            res = max(res, total)
-
-            while q and total >= q[-1][1]:
-                q.pop()
+        dq = deque()
+        for i in range(len(nums)):
+            nums[i] += nums[dq[0]] if dq else 0
             
-            if total > 0:
-                q.append((i, total))
-            
-            if q and q[0][0] == i - k:
-                q.popleft()
-        
-        return res 
+            while dq and (i - dq[0] >= k or nums[i] >= nums[dq[-1]]):
+                dq.pop() if nums[i] >= nums[dq[-1]] else dq.popleft()
+                
+            if nums[i] > 0:
+                dq.append(i)
+                
+        return max(nums)
