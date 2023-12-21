40959adc65fa43a4bc6328265d6ddbfd1f74c32e
Mihir Chowdhury <89465612+mhr1812@users.noreply.github.com>
2023-12-21 10:37:04 +0530

    Time: 3 ms (35.43%), Space: 8 MB (68.60%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -11,15 +11,17 @@ public:
 //     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n,0);
-        dp[0] = nums[0];
+        //vector<int> dp(n,0);
+        int prev2 = 0,prev=nums[0];
         for(int i=1;i<n;i++){
             int pick = nums[i];
             if(i>=2)
-                pick+=dp[i-2];
-            int notpick = 0+dp[i-1];
-            dp[i] = max(pick,notpick);
+                pick+=prev2;
+            int notpick = 0+prev;
+            int curi = max(pick,notpick);
+            prev2 = prev;
+            prev = curi;
         }
-        return dp[n-1];
+        return prev;
     }
 };
